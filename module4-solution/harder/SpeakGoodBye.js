e1117a53b40ead729e2d6dbd8e10e7272629bb2f
Oliveiras96 <caique.campos@aluno.ufabc.edu.br>
2020-07-30 11:50:20 -0300

    module 4 solution

---
+++ module4-solution/harder/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+//IIFE:
+(function (window){
+
+var byeSpeaker = {};
+var speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window); //End of IIFE
